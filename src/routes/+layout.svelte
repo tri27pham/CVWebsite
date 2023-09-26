f7d8c3b48c6fdb9783c5b3c98e37a0bffa5d95f8
tri27pham <tri.pham2002@outlook.com>
2023-09-26 17:48:29 +0100

    experiment w understanding tailwind

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,11 @@
     import "../app.css";
   </script>
   
-<div>
+<div class="p-8 max-w-6xl mx-auto">
     <slot></slot>
 </div>
+<style>
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+</style>
