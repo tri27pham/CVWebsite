9a7a444eaac485da45dc07afd1380654a5837ebc
tri27pham <tri.pham2002@outlook.com>
2023-09-26 17:52:26 +0100

    create different sections

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,24 @@
 </svelte:head>
 
 <h3 class="text-center text-6xl text-white font-mono">Tri Pham</h3>
-<p class="p-4 text-center text-white text-2xl font-mono">About me</p>
+<p class="p-4 text-center text-white text-3xl font-mono">
+    About me
+</p>
+<p class="p-2 text-base text-white font-mono">
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+</p>
+<p class="p-4 text-center text-white text-3xl font-mono">
+    Academics
+</p>
+<p class="p-2 text-base text-white font-mono">
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+</p>
+<p class="p-4 text-center text-white text-3xl font-mono">
+    Projects
+</p>
+<p class="p-2 text-base text-white font-mono">
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+</p>
 
 <style lang="postcss">
     :global(html) {
