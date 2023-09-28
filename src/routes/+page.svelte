4af8b95b2d47fd0d76d8e198dc784caeff310a66
tri27pham <tri.pham2002@outlook.com>
2023-09-28 20:08:07 +0100

    add icons and chane colouring

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,6 +6,11 @@
     import tailwind from '../images/tailwind.png';
     import javascript from '../images/javascript.png';
     import python from '../images/python.png';
+    import noSql from '../images/noSql.png';
+    import sql from '../images/sql.png';
+    import flutter from '../images/flutter.png';
+    
+
 
     import { fade } from 'svelte/transition';
 
@@ -32,8 +37,6 @@
     <title>Tri Pham</title>
 </svelte:head>
 
-    
-<div id="top" class="flex flex-col">
     
     <!-- LOADING SCREEN -->
     {#await loadingScreenVisible then value}
@@ -74,11 +77,13 @@
     {#await pageVisible then value}
         {#if value}
 
+    <div id="top" class="flex flex-col">
+
         <nav class="text-white font-mono">
 
             <div class="z-50 fixed top-7 left-10 text-xl hover:scale-125 transition-transform duration-500  hover:text-purple">
                 <a 
-                    href="#top">
+                    href="#MainPage">
                     Tri Pham
                 </a>
             </div>
@@ -107,51 +112,47 @@
             </div>
         </nav>
 
-        <div>
-            <div class="mt-96"></div>
-            
-            <div class="w-max">
+        <div id="MainPage">
+            <div class=" mt-96"></div>
+            <div class="w-max " style=" text-align: left;">
                 <h3 class=" p-4 text-center animate-typing overflow-hidden whitespace-nowrap 
                     hover:scale-110 transition duration-500
                     border-r-4 border-r-white pr-5 text-7xl text-white font-mono">
                     Hi, I'm <span style=" color:blueviolet;">Tri</span>.
                 </h3>
-                <h2 class="p-4 text-white font-mono text-3xl animate-typing overflow-hidden whitespace-nowrap
+                <h2 class="p-4 text-white font-mono text-2xl animate-typing overflow-hidden whitespace-nowrap
                 hover:scale-110 transition duration-500">
-                    Computer Science student
+                    <span style=" color:blueviolet;">Masters student</span> - Computer Science 
                 </h2>
             </div>
-
-            <div class="mt-96"></div>
+            <div class=" mt-52"></div>
         </div>
 
-        <div class="mt-96"></div>
-        <div class="mt-96"></div>
-        <div class="mt-96"></div>
+        <div id="AboutMe">
 
+            <div class="mt-96"></div>
 
-        <div id="AboutMe" class="mb-96 scrollable animate-fade-in">
-
-            <div class="mt-72"></div>
-            
-            <div class="flex flex-row ">
-                <div class="flex flex-col items-center">
+            <div class="flex flex-row space-x-8 ">
+                <div class="flex flex-col items-center w-1/2">
                     <p class="p-4 text-center text-white text-6xl font-mono">
                         About me
                     </p>
-                    <p class="p-1 max-w-full text-center text-blueViolet text-3xl font-mono">
+                    <p class="p-1 max-w-full text-center text-white text-3xl font-mono hover:text-purple hover:scale-125 transition-transform duration-500">
                         Tri Pham
                     </p>
                     <p class="p-1 max-w-full text-center text-white text-heading2 font-mono">
                         Computer Science undergradudate
                     </p>
                     <p class="p-1 max-w-full text-center text-white text-xl font-mono">
-                        I am third year student currently pursuing an <br>
-                        integrated  <span style=" color:blueViolet;">Masters</span> at King's College London.
+                        I am third year student currently pursuing an 
+                        integrated Masters at King's College London, my year
+                        of graduation will be 2025. 
+                        Presently, I am aiming to specialise in Artifical Intelligence,
+
                     </p>
                 </div>
-                
-                <div class="flex flex-wrap items-center">
+
+                <div class="flex flex-wrap items-center w-1/2">
                     <p class="h-20 bg-charcoal px-4 py-2 mr-2 mt-2 text-gray-400 rounded text-base font-mono font-semibold flex flex-col items-center justify-center gap-2 hover:scale-110 duration-150">
                         <img src={svelte} alt="svelte" width="25">
                         Sveltekit
@@ -168,13 +169,28 @@
                         <img src={python} alt="svelte" width="30">
                         Python
                     </p>
+                    <p class="h-20 bg-charcoal px-4 py-2 mr-2 mt-2 text-gray-400 rounded text-base font-mono font-semibold flex flex-col items-center justify-center gap-2 hover:scale-110 duration-150">
+                        <img src={noSql} alt="svelte" width="30">
+                        NoSQL
+                    </p>
+                    <p class="h-20 bg-charcoal px-4 py-2 mr-2 mt-2 text-gray-400 rounded text-base font-mono font-semibold flex flex-col items-center justify-center gap-2 hover:scale-110 duration-150">
+                        <img src={sql} alt="svelte" width="30">
+                        SQL
+                    </p>
+                    <p class="h-20 bg-charcoal px-4 py-2 mr-2 mt-2 text-gray-400 rounded text-base font-mono font-semibold flex flex-col items-center justify-center gap-2 hover:scale-110 duration-150">
+                        <img src={flutter} alt="svelte" width="30">
+                        Flutter
+                    </p>
+    
     
                 </div>
             </div>
             
+            <div class="mt-52"></div>
+
         </div>
     
-        <div id="Academics" class=" mb-96">
+        <div id="Academics">
             <div class="mt-96"></div>
             <div class="flex flex-col hover:scale-105 scrollable animate-fade-in">
                 <p class="p-4 text-center text-white text-3xl font-mono">
@@ -184,10 +200,11 @@
                     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
                 </p>
             </div>
+            <div class="mt-52"></div>
         </div>
     
-        <div id="Projects" class="mb-96">
-            <div class="mt-96"></div>
+        <div id="Projects">
+            <div class="mt-52"></div>
             <div>
                 <p class="p-4 text-center text-white text-3xl font-mono">
                     Projects
@@ -196,11 +213,17 @@
                     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
                 </p>
             </div>
+            <div class="mt-96"></div>
         </div>
+
+ 
+     </div>
+        
+
         {/if}
     {/await}
 
-</div>
+
 
 
 <style lang="postcss">
