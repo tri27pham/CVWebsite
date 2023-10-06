cd9307967dffe1b83b95902b22ef7568458e0fa7
tri27pham <tri.pham2002@outlook.com>
2023-10-06 20:41:38 +0100

    delete unecessary files

--- src/components/scroll-bar.svelte
+++
@@ -1,9 +0,0 @@
-<style>
-    /* Add Tailwind CSS classes to style your vertical bar */
-    .scroll-bar {
-      @apply p-2 fixed top-0 left-0 h-4/5 bg-white; /* Adjust classes as needed */
-      /* width: 1%; */
-    }
-  </style>
-
-<div class="scroll-bar" style="width: 1%;"></div>
